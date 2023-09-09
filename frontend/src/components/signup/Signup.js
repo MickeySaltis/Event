0380019ba1595466869f94001ea6f1233389ea58
MickeySaltis <cogeta4@hotmail.fr>
2023-09-09 11:07:40 +0200

    Up Signup template

--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lastName: "",
+            firstname: "",
+            email: "",
+            password: "",
+            avatar: ""
+        }
+    }
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+    onSignupClick = () => {
+        const userData = {
+            lastName: this.state.lastName,
+            firstname: this.state.firstname,
+            email: this.state.email,
+            password: this.state.password,
+            avatar: this.state.avatar[0]
+        };
+        console.log("Inscription: " + userData.lastName + " " + userData.firstname + " " + userData.email + " " + userData.password);
+    }
+
     render() {
         return (
             <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800 m-24">
@@ -15,34 +40,40 @@ class Signup extends Component {
 
                     {/* Nom */}
                     <div className="mb-6">
-
+                        <input type="text" id="lastNameId" name="lastName" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Nom" value={this.state.lastName} onChange={this.onChange}
+                            required />
                     </div>
 
                     {/* Prénom */}
                     <div className="mb-6">
-
+                        <input type="text" id="firstnameId" name="firstname" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Prénom" value={this.state.firstname} onChange={this.onChange}
+                            required />
                     </div>
 
                     {/* Email */}
                     <div className="mb-6">
-
+                        <input type="email" id="emailId" name="email" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Adresse email" value={this.state.email} onChange={this.onChange}
+                            required />
                     </div>
 
                     {/* Mot de passe */}
                     <div className="mb-4">
-
+                        <input type="password" id="passwordId" name="password" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Mot de passe" value={this.state.password} onChange={this.onChange}
+                            required />
                     </div>
 
                     {/* Avatar */}
                     <div className="mb-6">
-                        <label className="block text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Avatar</label>
-
+                        <label className="block text-sm font-medium text-gray-900 dark:text-white" >Avatar</label>
+                        <input type="file" id="avatarId" name="avatar" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Avatar" accept="image/png, image/jpeg" value={this.state.avatar[0]} onChange={this.onChange} />
                         <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PNG ou JPG (MAX. 1024k).</p>
                     </div>
 
                     {/* Soumission */}
                     <div>
-
+                        <button type="submit" className="text-white inline-block px-7 py-3 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80active:shadow-lg transition duration-150 ease-in-out w-full" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={this.onSignupClick}>
+                            Inscription
+                        </button>
                     </div>
 
                 {/* Fin form */}
